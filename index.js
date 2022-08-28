cbc829a408e7f0ce8c9acc261e8432dc29f1c686
Kidrash <markkaranja11@gmail.com>
2022-08-28 22:02:43 +0300

    Completed asignment

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
   }
   function moveDodgerRight() {
-    const rightNumbers = dodger.style.right.replace("", "px");
-    const right = parseInt(rightNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   
-    if (right > 0) {
-      dodger.style.right = `${right + 1}px`;
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
     }
   }
